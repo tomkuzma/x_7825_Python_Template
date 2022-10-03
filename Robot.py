d81015d239d495c82cae144d58f494b91d0598db
Tom Kuzma <t_kuzma@hotmail.com>
2022-10-02 21:18:33 -0700

    First Commit

---
+++ Robot.py
@@ -0,0 +1,3 @@
+class PRobot:
+    def __init__(self) -> None:
+        super().__init__()
---
+++ main.py
@@ -0,0 +1,24 @@
+# This is a sample Python script.
+from platform import python_version
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+import cv2 as cv, numpy as np
+
+
+def print_hello(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hello('PyCharm')
+
+    # T = np.array([1, 0, 0]
+    #              [1, 1, 1]
+    #              [0, 1, 1])
+
+    print(python_version())
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
